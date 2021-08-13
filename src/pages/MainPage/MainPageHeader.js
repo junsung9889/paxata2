1b67bf31f5d7d1d62e12e7fb8122a3227efb8681
junsung9889 <junsung9889@gmail.com>
2021-08-13 09:16:38 +0900

    update #2

--- src/pages/MainPage/MainPageHeader.js
+++ src/pages/MainPage/MainPageHeader.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function MainPageHeader(){
     return(
         <div className = "mainHeader">
-            DataSet
+            <h1 className = 'headerTitle'>DataSet</h1>
             <Link to = '/import'>
                 <Button className = "main-btn">Import</Button>
             </Link>
