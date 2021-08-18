ca67483a9359caf980a6e532816b6c452e9b4c92
yuma <tenlifoxx@gmail.com>
2021-08-18 10:45:33 +0900

    filter

--- src/pages/MainPage/FilterModal.js
+++ src/pages/MainPage/FilterModal.js
@@ -1,11 +1,47 @@
 import { Modal, Button } from 'react-bootstrap';
 import { useState } from 'react';
+import { Form, FormControl } from 'react-bootstrap';
 
-export default function FilterModal() {
+
+function SearchOptions(){
+    function handleCategory(){
+
+    }
+
+    function handleGL(){
+
+    }
+
+    function handleValue(){
+
+    }
+
+    return(
+      <Form.Group>
+          Form place
+      </Form.Group>
+    );
+}
+
+
+export default function FilterModal(props) {
   const [show, setShow] = useState(false);
+  const [filter, setFilter] = useState([]);
+
+  let key = 0;
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const {data} = props;
+  const {setData} = props;
+  const {filterOpt} = props;
+
+  function clickAdd(){
+    setFilter(filter.concat({'key': key, 'category': '', 'gl': '', 'value': 0}));
+    console.log(filter);
+  }
+
 
   return (
     <>
@@ -13,15 +49,21 @@ export default function FilterModal() {
         Filter
       </Button>
 
-      <Modal show={show} onHide={handleClose} centered>
+      <Modal show={show} onHide={handleClose} backdrop={'static'}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Filter Settings</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+            <Form>
+                {filter.map((f) =>
+                <SearchOptions key={f.key}/>)}
+            </Form>
+            <div style={{display: 'flex'}}>
+                <Button onClick={clickAdd} style={{marginLeft: 'auto'}}>+</Button>
+            </div>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
+
           <Button variant="warning" onClick={handleClose}>
             Apply
           </Button>
