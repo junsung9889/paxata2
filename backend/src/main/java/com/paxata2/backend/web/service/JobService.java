cdc473e1b7871a269aa401e5525e43144d55ab2a
junsung9889 <junsung9889@gmail.com>
2021-09-28 15:52:02 +0900

    quartz #1

---
+++ backend/src/main/java/com/paxata2/backend/web/service/JobService.java
@@ -0,0 +1,33 @@
+package com.paxata2.backend.web.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class JobService {
+
+    public static final long EXECUTION_TIME = 5000L;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private AtomicInteger count = new AtomicInteger();
+
+    public void executeSampleJob() {
+
+        logger.info("The sample job has begun...");
+        try {
+            Thread.sleep(EXECUTION_TIME);
+        } catch (InterruptedException e) {
+            logger.error("Error while executing sample job", e);
+        } finally {
+            count.incrementAndGet();
+            logger.info("Sample job has finished...");
+        }
+    }
+
+    public int getNumberOfInvocations() {
+        return count.get();
+    }
+}
